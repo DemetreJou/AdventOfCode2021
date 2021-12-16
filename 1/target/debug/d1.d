e954a90c8609d2c28396466729ebef47334b2b77
Demetre <demetre.jouras@gmail.com>
2021-12-15 23:03:56 -0500

    remove compile files

--- 1/target/debug/d1.d
+++
@@ -1,1 +0,0 @@
-/home/demetre/CLionProjects/AdventOfCode2021/1/target/debug/d1: /home/demetre/CLionProjects/AdventOfCode2021/1/src/main.rs
--- 1/target/debug/deps/d1-b503c99dd1d4cac4.d
+++
@@ -1,5 +0,0 @@
-/home/demetre/CLionProjects/AdventOfCode2021/1/target/debug/deps/d1-b503c99dd1d4cac4: src/main.rs
-
-/home/demetre/CLionProjects/AdventOfCode2021/1/target/debug/deps/d1-b503c99dd1d4cac4.d: src/main.rs
-
-src/main.rs:
