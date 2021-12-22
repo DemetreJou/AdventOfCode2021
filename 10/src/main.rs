3c6d3a7a02dc051b8ec97adf1ecef28239e56af2
Demetre <demetre.jouras@gmail.com>
2021-12-21 22:19:25 -0500

    add day 10 part 1

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -1,41 +1,45 @@
-use std::collections::{HashMap, HashSet};
-use std::collections::hash_map::RandomState;
+use std::collections::HashMap;
 use std::fs;
 
-static INPUT_FILE: &str = "test_input.txt";
-
+static INPUT_FILE: &str = "input.txt";
 
 fn part_1(raw_input: &str) {
-    let h: HashMap<&str, i32> = [
-        (")", 3),
-        ("]", 57),
-        ("}", 1197),
-        (">", 25137)
-    ]
+    let h: HashMap<&str, i32> = [(")", 3), ("]", 57), ("}", 1197), (">", 25137)]
+        .iter()
+        .cloned()
+        .collect();
+
+    let opposite: HashMap<&str, &str> = [(")", "("), ("]", "["), ("}", "{"), (">", "<")]
         .iter()
         .cloned()
         .collect();
 
     let mut input = Vec::new();
-    raw_input.trim()
-        .lines()
-        .for_each(|l| {
-            input.push(
-                l.clone().chars().as_str()
-            );
-        });
+    for line in raw_input.lines() {
+        for ch in line.chars() {
+            input.push(ch.to_string());
+        }
+    }
 
-    println!("{:?}", input);
     let mut stack: Vec<&str> = Vec::new();
     let mut score = 0;
-    input.iter().for_each(|c| {
+    input.iter().for_each(|c_0| {
+        let c  = &c_0.as_str();
         if *c == "(" || *c == "[" || *c == "{" || *c == "<" {
             stack.push(c);
-        } else if stack.pop().unwrap() == *c {
-            // intentionally blank
-        } else {
-            score += h.get(c).unwrap();
-            panic!("Unbalanced brackets");
+        }
+        else {
+            let matching_bracket = *opposite.get(c).unwrap();
+            match stack.pop(){
+                Some(x) => {
+                if x == matching_bracket {
+
+                    } else {
+                        score += h.get(c).unwrap();
+                    }
+                }
+                None => {}
+            }
         }
     });
     println!("{}", score);
